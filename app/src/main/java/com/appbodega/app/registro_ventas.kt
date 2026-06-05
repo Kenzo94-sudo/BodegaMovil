06abfbea41507e8b38c8144b294ceafa30851da6
DeveloperYanRex <yandelpomacarbajal043@gmail.com>
2026-06-05 10:51:15 -0500

    Ese agrego el registro de ventas y unos cambios

--- app/src/main/java/com/appbodega/app/registro_ventas.kt
+++ app/src/main/java/com/appbodega/app/registro_ventas.kt
@@ -1,17 +1,16 @@
-package com.appbodega.mobile
+package com.appbodega.app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.appbodega.app.R
 
-class catalogo_bebidas : AppCompatActivity() {
+class registro_ventas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_catalogo_bebidas)
+        setContentView(R.layout.activity_registro_ventas)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
