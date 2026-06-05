65d29aad01e8be0e6b0e7bb2c3d7da9e74c76eee
DeveloperYanRex <yandelpomacarbajal043@gmail.com>
2026-06-05 13:02:06 -0500

    cambios en los botones algunas funcionalidades de interacciones

--- app/src/main/java/com/appbodega/app/catalogo_limpieza.kt
+++ app/src/main/java/com/appbodega/app/catalogo_limpieza.kt
@@ -1,16 +1,24 @@
 package com.appbodega.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class catalogo_limpieza : AppCompatActivity() {
+    private lateinit var btnAtras: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_catalogo_limpieza)
+        btnAtras=findViewById(R.id.btnAtras)
+        btnAtras.setOnClickListener {
+            var intent= Intent(this, catalogo_bodega::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
