99b70de3bdef8ff0989aa70c301c4fb06303bf52
Kenzo <A20100877@idat.pe>
2026-06-24 02:29:20 -0500

    Menu y el recyclerView faltante

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.core.ktx)
+    implementation(libs.play.services.maps3d)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
